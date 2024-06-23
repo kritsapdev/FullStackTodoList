75cc5cac22fb1a4e0be541873acf9d969ffbab93
kritsapdev <kritsapdev@gmail.com>
2024-06-24 01:07:07 +0700

    put

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,21 @@ app.get("/todo",(req,res)=>{
   res.json(tasks);
 });
 
+app.get("/todo/:id",(req,res)=>{
+  res.json({msg:"wow this is ID 1"});
+});
 
+app.post("/todo",(req,res)=>{
+  res.json({msg:"Add Todo"});
+});
+
+app.put("/todo",(req,res)=>{
+  res.json({msg:"Update Todo"});
+});
+
+app.delete("/todo",(req,res)=>{
+  res.json({msg:"Delete Todo"});
+});
 
 app.listen(PORT,()=>{
   console.log(`App is running on PORT ${PORT}`);
